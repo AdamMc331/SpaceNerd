96c64fc45aca5c5a28ba6695c28a3c38349156f3
Adam McNeilly <amcneilly331@gmail.com>
2025-05-26 18:37:39 -0400

    Adding stubs for each screen. (#6)

---
+++ app/src/main/java/com/adammcneilly/spacenerd/screens/astronauts/AstronautListScreen.kt
@@ -0,0 +1,32 @@
+package com.adammcneilly.spacenerd.screens.astronauts
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.adammcneilly.spacenerd.scaffold.PersistentScaffold
+import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationBar
+import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationRail
+import com.adammcneilly.spacenerd.scaffold.rememberScaffoldState
+
+@Composable
+@OptIn(ExperimentalSharedTransitionApi::class)
+fun AstronautListScreen(modifier: Modifier = Modifier) {
+    rememberScaffoldState().PersistentScaffold(
+        modifier = modifier,
+        navigationBar = {
+            PersistentNavigationBar()
+        },
+        navigationRail = {
+            PersistentNavigationRail()
+        },
+        content = { scaffoldPadding ->
+            Text(
+                text = "Astronaut List Screen",
+                modifier = Modifier
+                    .padding(scaffoldPadding),
+            )
+        },
+    )
+}
---
+++ app/src/main/java/com/adammcneilly/spacenerd/screens/launches/LaunchListScreen.kt
@@ -0,0 +1,32 @@
+package com.adammcneilly.spacenerd.screens.launches
+
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.adammcneilly.spacenerd.scaffold.PersistentScaffold
+import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationBar
+import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationRail
+import com.adammcneilly.spacenerd.scaffold.rememberScaffoldState
+
+@Composable
+@OptIn(ExperimentalSharedTransitionApi::class)
+fun LaunchListScreen(modifier: Modifier = Modifier) {
+    rememberScaffoldState().PersistentScaffold(
+        modifier = modifier,
+        navigationBar = {
+            PersistentNavigationBar()
+        },
+        navigationRail = {
+            PersistentNavigationRail()
+        },
+        content = { scaffoldPadding ->
+            Text(
+                text = "Launch List Screen",
+                modifier = Modifier
+                    .padding(scaffoldPadding),
+            )
+        },
+    )
+}
