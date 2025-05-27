2a1248a5a56a6a41a431d7215c9908590450210e
Adam McNeilly <amcneilly331@gmail.com>
2025-05-26 22:47:54 -0400

    Handling back navigation.

--- app/src/main/java/com/adammcneilly/spacenerd/screens/astronauts/AstronautListScreen.kt
+++ app/src/main/java/com/adammcneilly/spacenerd/screens/astronauts/AstronautListScreen.kt
@@ -2,21 +2,34 @@ package com.adammcneilly.spacenerd.screens.astronauts
 
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.adammcneilly.spacenerd.scaffold.PersistentScaffold
 import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationBar
 import com.adammcneilly.spacenerd.scaffold.navigation.components.PersistentNavigationRail
 import com.adammcneilly.spacenerd.scaffold.rememberScaffoldState
+import com.adammcneilly.spacenerd.scaffold.R as scaffoldR
 
 @Composable
-@OptIn(ExperimentalSharedTransitionApi::class)
+@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalMaterial3Api::class)
 fun AstronautListScreen(
     modifier: Modifier = Modifier,
 ) {
     rememberScaffoldState().PersistentScaffold(
         modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(scaffoldR.string.astronauts),
+                    )
+                },
+            )
+        },
         navigationBar = {
             PersistentNavigationBar()
         },
