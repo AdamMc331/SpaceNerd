11a1b5018b1d2cdfac3ad51e490f26169212d20f
Adam McNeilly <amcneilly331@gmail.com>
2025-05-31 13:04:53 -0400

    Removing more theme stuff.

--- scaffold/src/main/java/com/adammcneilly/spacenerd/scaffold/ui/theme/Theme.kt
+++ scaffold/src/main/java/com/adammcneilly/spacenerd/scaffold/ui/theme/Theme.kt
@@ -2,22 +2,10 @@ package com.adammcneilly.spacenerd.scaffold.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-
-private val darkColorScheme = darkColorScheme(
-    primary = Purple200,
-    secondary = Teal200,
-)
-
-private val lightColorScheme = lightColorScheme(
-    primary = Purple500,
-    secondary = Teal200,
-)
 
 @Composable
 fun SpaceTheme(
