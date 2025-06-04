bbd66e9eccff0e6d30ac5f543d9386a02f8a04c2
Adam McNeilly <amcneilly331@gmail.com>
2025-06-04 13:45:58 -0400

    Adding news feature module.

---
+++ feature/news/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    )
+
+    sourceSets {
+        commonMain.dependencies {
+
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
+
+tasks.formatKotlinCommonMain {
+    exclude { it.file.path.contains("build/") }
+}
+
+tasks.lintKotlinCommonMain {
+    exclude { it.file.path.contains("build/") }
+}
