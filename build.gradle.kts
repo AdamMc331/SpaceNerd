21d1188f43041104f51cda38fef039c261f5b4af
Adam McNeilly <amcneilly331@gmail.com>
2025-06-08 00:26:00 -0400

    Fixing detekt check.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.gradle.kotlin.dsl.withType
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import org.jmailen.gradle.kotlinter.tasks.LintTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
@@ -23,6 +27,14 @@ subprojects {
     apply(plugin = "com.squareup.sort-dependencies")
     apply(plugin = "io.gitlab.arturbosch.detekt")
     apply(plugin = "org.jmailen.kotlinter")
+
+    tasks.withType<FormatTask> {
+        exclude { it.file.path.contains("build/") }
+    }
+
+    tasks.withType<LintTask> {
+        exclude { it.file.path.contains("build/") }
+    }
 }
 
 tasks.register("clean", Delete::class) {
