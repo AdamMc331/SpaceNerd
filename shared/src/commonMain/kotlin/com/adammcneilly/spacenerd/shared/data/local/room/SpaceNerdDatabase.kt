b87494928add3d55cfa793a96f8d7baccd2d6575
Adam McNeilly <amcneilly331@gmail.com>
2025-06-12 00:26:46 -0700

    Adding room cache timestamp repo. (#59)

--- shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/data/local/room/SpaceNerdDatabase.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/data/local/room/SpaceNerdDatabase.kt
@@ -6,16 +6,21 @@ import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import com.adammcneilly.spacenerd.shared.data.article.local.room.RoomArticleDTO
 import com.adammcneilly.spacenerd.shared.data.article.local.room.RoomArticleDao
+import com.adammcneilly.spacenerd.shared.data.cache.local.room.RoomCacheTimestampDTO
+import com.adammcneilly.spacenerd.shared.data.cache.local.room.RoomCacheTimestampDao
 
 @Database(
     version = 1,
     entities = [
         RoomArticleDTO::class,
+        RoomCacheTimestampDTO::class,
     ],
 )
 @ConstructedBy(SpaceNerdDatabaseConstructor::class)
 abstract class SpaceNerdDatabase : RoomDatabase() {
     abstract fun articleDao(): RoomArticleDao
+
+    abstract fun cacheTimestampDao(): RoomCacheTimestampDao
 }
 
 /**
