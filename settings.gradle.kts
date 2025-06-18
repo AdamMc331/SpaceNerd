5e731c9dbf873ede37b266bb4503152796033f0c
Adam McNeilly <amcneilly331@gmail.com>
2025-06-18 19:54:02 -0400

    Updating paparazzi snapshots.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 rootProject.name = "Space Nerd"
