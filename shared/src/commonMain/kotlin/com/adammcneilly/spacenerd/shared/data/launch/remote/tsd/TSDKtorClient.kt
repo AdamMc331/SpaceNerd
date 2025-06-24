6c8f23d82944443b9f015368e947a2aa0cdda962
Adam McNeilly <amcneilly331@gmail.com>
2025-06-23 21:20:21 -0400

    Updating base url for TSD API. (#92)

--- shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/data/launch/remote/tsd/TSDKtorClient.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/data/launch/remote/tsd/TSDKtorClient.kt
@@ -2,4 +2,4 @@ package com.adammcneilly.spacenerd.shared.data.launch.remote.tsd
 
 import com.adammcneilly.spacenerd.shared.data.remote.ktor.BaseKtorClient
 
-object TSDKtorClient : BaseKtorClient("https://ll.thespacedevs.com/")
+object TSDKtorClient : BaseKtorClient("https://ll.thespacedevs.com/2.3.0/")
