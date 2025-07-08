b0372ee634596b81463d8c70ad4280cbdb039f42
Adam McNeilly <amcneilly331@gmail.com>
2025-07-08 08:58:20 -0400

    Testing news screen. (#110)

---
+++ shared/src/test/kotlin/com/adammcneilly/spacenerd/shared/feature/news/ui/NewsContentPaparazziTest.kt
@@ -0,0 +1,46 @@
+package com.adammcneilly.spacenerd.shared.feature.news.ui
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.ui.unit.dp
+import com.adammcneilly.spacenerd.shared.BasePaparazziTest
+import com.adammcneilly.spacenerd.shared.core.displaymodels.ArticleDisplayModel
+import com.adammcneilly.spacenerd.shared.core.models.Article
+import com.adammcneilly.spacenerd.shared.core.models.Author
+import kotlinx.datetime.Instant
+import kotlin.test.Test
+
+class NewsContentPaparazziTest : BasePaparazziTest() {
+    @Test
+    fun renderDefault() {
+        val articles = List(10) { index ->
+            Article(
+                id = index.toString(),
+                title = "Neil Armstrong Steps Foot On The Moon",
+                imageUrl = "",
+                url = "",
+                summary = "Armstrong, Aldrin, and Collins complete successful Apollo 11 mission.",
+                publishedAtUtc = Instant.parse("2025-06-15T22:01:57Z"),
+                authors = listOf(
+                    Author(
+                        name = "NASA",
+                    ),
+                ),
+            )
+        }
+
+        val displayModels = articles.map(::ArticleDisplayModel)
+
+        val state = NewsUiState(
+            articles = displayModels,
+            selectedArticle = null,
+        )
+
+        snapshot(screenPaddingDp = 0) {
+            NewsContent(
+                state = state,
+                contentPadding = PaddingValues(16.dp),
+                onEvent = {},
+            )
+        }
+    }
+}
