85fb10b6cedd652c3eca4a374484a9e14472e94d
Adam McNeilly <amcneilly331@gmail.com>
2025-07-12 23:47:17 -0400

    Fixing template.

--- data/remote/ktor/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/ktor/BaseKtorClient.kt
+++ data/remote/ktor/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/ktor/BaseKtorClient.kt
@@ -9,6 +9,7 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.SIMPLE
 import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
