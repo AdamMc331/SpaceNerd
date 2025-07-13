6326ac9e92c07ab961262e344264c86a5ce9c4b5
Adam McNeilly <amcneilly331@gmail.com>
2025-07-12 23:46:33 -0400

    Fixing template.

--- core/models/build.gradle.kts
+++ core/models/build.gradle.kts
@@ -22,3 +22,21 @@ kotlin {
         }
     }
 }
+
+android {
+    compileSdk = libs.versions.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    namespace = "com.adammcneilly.spacenerd.core.models"
+}
