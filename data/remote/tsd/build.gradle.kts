4797cfb5b3419b1f2715e8636098b967f6d9fe43
Adam McNeilly <amcneilly331@gmail.com>
2025-07-12 22:58:00 -0400

    Adding TSD module.

---
+++ data/remote/tsd/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    )
+
+    sourceSets {
+        commonMain.dependencies {
+
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,4 @@ dependencyResolutionManagement {
 rootProject.name = "Space Nerd"
 include(":app")
 include(":shared")
+include(":data:remote:tsd")
