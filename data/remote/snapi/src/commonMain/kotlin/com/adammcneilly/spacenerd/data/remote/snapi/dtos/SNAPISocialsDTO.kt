9e2201eed53d1aabea2c0a711b6833a04e848db9
Adam McNeilly <amcneilly331@gmail.com>
2025-07-14 23:08:30 -0400

    Adding snapi module.

---
+++ data/remote/snapi/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/snapi/dtos/SNAPISocialsDTO.kt
@@ -0,0 +1,20 @@
+package com.adammcneilly.spacenerd.data.remote.snapi.dtos
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SNAPISocialsDTO(
+    @SerialName(value = "bluesky")
+    val bluesky: String? = null,
+    @SerialName(value = "instagram")
+    val instagram: String? = null,
+    @SerialName(value = "linkedin")
+    val linkedIn: String? = null,
+    @SerialName(value = "mastodon")
+    val mastodon: String? = null,
+    @SerialName(value = "x")
+    val x: String? = null,
+    @SerialName(value = "youtube")
+    val youtube: String? = null,
+)
