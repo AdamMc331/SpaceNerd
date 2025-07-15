ac3df735f16435e37cbd89755868f87289efcb5a
Adam McNeilly <amcneilly331@gmail.com>
2025-07-14 23:09:37 -0400

    Adding launch data module.  (#128)

---
+++ data/launch/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/launch/LaunchListRequest.kt
@@ -0,0 +1,14 @@
+package com.adammcneilly.spacenerd.data.launch
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
+
+sealed interface LaunchListRequest {
+    data object Upcoming : LaunchListRequest
+
+    @OptIn(ExperimentalTime::class)
+    data class Custom(
+        val before: Instant? = null,
+        val after: Instant? = null,
+    ) : LaunchListRequest
+}
