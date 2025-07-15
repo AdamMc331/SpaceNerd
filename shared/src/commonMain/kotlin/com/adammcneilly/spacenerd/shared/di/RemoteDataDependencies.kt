75548431a7a01f0a945c308f3578c440f60f6b72
Adam McNeilly <amcneilly331@gmail.com>
2025-07-15 00:14:12 -0400

    Shared module cleanup (#130)

--- shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/di/RemoteDataDependencies.kt
+++ shared/src/commonMain/kotlin/com/adammcneilly/spacenerd/shared/di/RemoteDataDependencies.kt
@@ -1,13 +1,13 @@
 package com.adammcneilly.spacenerd.shared.di
 
+import com.adammcneilly.spacenerd.data.agency.remote.RemoteAgencyService
+import com.adammcneilly.spacenerd.data.agency.remote.tsd.TSDAgencyService
+import com.adammcneilly.spacenerd.data.article.remote.RemoteArticleService
+import com.adammcneilly.spacenerd.data.article.remote.snapi.SNAPIArticleService
+import com.adammcneilly.spacenerd.data.launch.remote.RemoteLaunchService
+import com.adammcneilly.spacenerd.data.launch.remote.tsd.TSDLaunchService
+import com.adammcneilly.spacenerd.data.remote.snapi.SNAPIKtorClient
 import com.adammcneilly.spacenerd.data.remote.tsd.dtos.TSDKtorClient
-import com.adammcneilly.spacenerd.shared.data.agency.remote.RemoteAgencyService
-import com.adammcneilly.spacenerd.shared.data.agency.remote.tsd.TSDAgencyService
-import com.adammcneilly.spacenerd.shared.data.article.remote.RemoteArticleService
-import com.adammcneilly.spacenerd.shared.data.article.remote.snapi.SNAPIArticleService
-import com.adammcneilly.spacenerd.shared.data.article.remote.snapi.SNAPIKtorClient
-import com.adammcneilly.spacenerd.shared.data.launch.remote.RemoteLaunchService
-import com.adammcneilly.spacenerd.shared.data.launch.remote.tsd.TSDLaunchService
 import org.koin.dsl.module
 
 val remoteModule = module {
