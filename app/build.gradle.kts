943c905ac0d6151befbe58cf40d25d0b9b5a3d48
Adam McNeilly <amcneilly331@gmail.com>
2025-07-18 16:58:25 -0400

    Adding stationlist feature.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(project(":core:displaymodels"))
     implementation(project(":core:scaffold"))
     implementation(project(":data:local:room"))
+    implementation(project(":feature:stationlist"))
     implementation(project(":shared"))
     implementation(libs.android.material)
     implementation(libs.androidx.activity.compose)
