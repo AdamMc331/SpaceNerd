e59250b3214a58c6a777a3fa78a4ae9c80811600
Adam McNeilly <amcneilly331@gmail.com>
2025-07-19 15:00:09 -0400

    Feature module builds.

---
+++ feature/launchlist/src/commonMain/kotlin/com/adammcneilly/spacenerd/feature/launchlist/ui/LaunchListUiState.kt
@@ -0,0 +1,22 @@
+package com.adammcneilly.spacenerd.feature.launchlist.ui
+
+import com.adammcneilly.spacenerd.core.displaymodels.LaunchDisplayModel
+
+data class LaunchListUiState(
+    val launches: List<LaunchDisplayModel>,
+    val selectedLaunch: LaunchDisplayModel?,
+) {
+    companion object {
+        @Suppress("MagicNumber")
+        fun default(): LaunchListUiState {
+            val placeholderLaunches = List(3) {
+                LaunchDisplayModel.placeholder()
+            }
+
+            return LaunchListUiState(
+                launches = placeholderLaunches,
+                selectedLaunch = null,
+            )
+        }
+    }
+}
