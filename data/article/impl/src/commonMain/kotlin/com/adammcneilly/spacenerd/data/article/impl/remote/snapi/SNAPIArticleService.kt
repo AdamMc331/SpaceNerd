53c6b28d29efb5b00100987dfab87c77dc1765c8
Adam McNeilly <amcneilly331@gmail.com>
2025-07-19 22:17:04 -0400

    Impl module.

---
+++ data/article/impl/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/article/impl/remote/snapi/SNAPIArticleService.kt
@@ -0,0 +1,21 @@
+package com.adammcneilly.spacenerd.data.article.impl.remote.snapi
+
+import com.adammcneilly.spacenerd.core.models.Article
+import com.adammcneilly.spacenerd.data.article.api.remote.RemoteArticleService
+import com.adammcneilly.spacenerd.data.remote.ktor.BaseKtorClient
+import com.adammcneilly.spacenerd.data.remote.snapi.dtos.SNAPIArticleDTO
+import com.adammcneilly.spacenerd.data.remote.snapi.dtos.SNAPIArticleListResponseDTO
+
+class SNAPIArticleService(
+    private val client: BaseKtorClient,
+) : RemoteArticleService {
+    override suspend fun getArticles(): Result<List<Article>> {
+        return client.getResponse<SNAPIArticleListResponseDTO>(
+            endpoint = "v4/articles",
+        ).map { articleListResponse ->
+            articleListResponse.results
+                ?.map(SNAPIArticleDTO::toArticle)
+                .orEmpty()
+        }
+    }
+}
