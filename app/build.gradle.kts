8bba4687eb31fd4a90c6aed69e2126f298d3c74c
Adam McNeilly <amcneilly331@gmail.com>
2025-07-27 14:02:02 -0400

    Updating kotlinter.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -112,11 +112,3 @@ dependencies {
 
     kspAndroidTest(libs.hilt.android.compiler)
 }
-
-tasks.formatKotlinMain {
-    exclude { it.file.path.contains("build/")}
-}
-
-tasks.lintKotlinMain {
-    exclude { it.file.path.contains("build/")}
-}
