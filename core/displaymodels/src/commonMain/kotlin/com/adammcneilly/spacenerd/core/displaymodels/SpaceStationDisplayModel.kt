3af1efb8432fde9768b5fbce98486fe23ab4f04d
Adam McNeilly <amcneilly331@gmail.com>
2025-08-14 22:08:09 -0400

    Adding station summary card. (#178)

---
+++ core/displaymodels/src/commonMain/kotlin/com/adammcneilly/spacenerd/core/displaymodels/SpaceStationDisplayModel.kt
@@ -0,0 +1,32 @@
+package com.adammcneilly.spacenerd.core.displaymodels
+
+import com.adammcneilly.spacenerd.core.designsystem.models.ImageModel
+import com.adammcneilly.spacenerd.core.models.SpaceStation
+import com.adammcneilly.spacenerd.core.models.SpaceStationStatus
+
+data class SpaceStationDisplayModel(
+    val id: String,
+    val name: String,
+    val image: ImageModel,
+    val status: StatusDisplayModel,
+    val isPlaceholder: Boolean = false,
+) {
+    constructor(station: SpaceStation) : this(
+        id = station.id,
+        name = station.name,
+        image = ImageModel.Remote(station.imageUrl),
+        status = StatusDisplayModel(station.status),
+    )
+
+    companion object {
+        fun placeholder(): SpaceStationDisplayModel {
+            return SpaceStationDisplayModel(
+                id = "",
+                name = "Placeholder Station",
+                image = ImageModel.Placeholder,
+                status = StatusDisplayModel(SpaceStationStatus.Unknown),
+                isPlaceholder = true,
+            )
+        }
+    }
+}
