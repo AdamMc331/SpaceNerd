1bcef47f6bdc3a7a179ddddb60678e19b7540466
Adam McNeilly <amcneilly331@gmail.com>
2025-08-18 22:52:10 -0400

    Reverting change.

--- feature/launchlist/src/commonTest/kotlin/com/adammcneilly/spacenerd/feature/launchlist/LaunchListViewModelTest.kt
+++ feature/launchlist/src/commonTest/kotlin/com/adammcneilly/spacenerd/feature/launchlist/LaunchListViewModelTest.kt
@@ -62,6 +62,7 @@ class LaunchListViewModelTest {
             )
 
             viewModel.state.test {
+                assertThat(awaitItem()).isEqualTo(LaunchListUiState.default())
                 assertThat(awaitItem()).isEqualTo(expectedState)
 
                 cancelAndIgnoreRemainingEvents()
