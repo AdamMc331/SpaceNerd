6f1ed1c878c666ff6cf329cc4993d6078faf9fd0
Adam McNeilly <amcneilly331@gmail.com>
2025-08-18 22:37:18 -0400

    Using trimmed snapshots for nav components.

--- test/paparazzi/src/test/kotlin/com/adammcneilly/spacenerd/test/paparazzi/scaffold/components/BottomNavigationBarPaparazziTest.kt
+++ test/paparazzi/src/test/kotlin/com/adammcneilly/spacenerd/test/paparazzi/scaffold/components/BottomNavigationBarPaparazziTest.kt
@@ -1,20 +1,43 @@
 package com.adammcneilly.spacenerd.test.paparazzi.scaffold.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.spacenerd.core.designsystem.theme.SpaceTheme
 import com.adammcneilly.spacenerd.core.scaffold.app.AppState
 import com.adammcneilly.spacenerd.core.scaffold.app.LocalAppState
 import com.adammcneilly.spacenerd.core.scaffold.navigation.components.BottomNavigationBar
 import com.adammcneilly.spacenerd.test.paparazzi.BasePaparazziTest
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class BottomNavigationBarPaparazziTest : BasePaparazziTest() {
+@RunWith(TestParameterInjector::class)
+class BottomNavigationBarPaparazziTest {
+    @get:Rule
+    val paparazzi = Paparazzi()
+
+    @TestParameter
+    val useDarkTheme: Boolean = false
+
     @Test
     fun render() {
-        snapshot(screenPaddingDp = 0) {
+        paparazzi.snapshot {
             CompositionLocalProvider(
                 LocalAppState provides AppState(),
             ) {
-                BottomNavigationBar()
+                SpaceTheme(
+                    darkTheme = useDarkTheme,
+                ) {
+                    BottomNavigationBar()
+                }
             }
         }
     }
--- test/paparazzi/src/test/kotlin/com/adammcneilly/spacenerd/test/paparazzi/scaffold/components/SideNavigationRailPaparazziTest.kt
+++ test/paparazzi/src/test/kotlin/com/adammcneilly/spacenerd/test/paparazzi/scaffold/components/SideNavigationRailPaparazziTest.kt
@@ -1,24 +1,38 @@
 package com.adammcneilly.spacenerd.test.paparazzi.scaffold.components
 
 import androidx.compose.runtime.CompositionLocalProvider
+import app.cash.paparazzi.Paparazzi
+import com.adammcneilly.spacenerd.core.designsystem.theme.SpaceTheme
 import com.adammcneilly.spacenerd.core.scaffold.app.AppState
-import com.adammcneilly.spacenerd.core.scaffold.app.AppStateData
 import com.adammcneilly.spacenerd.core.scaffold.app.LocalAppState
-import com.adammcneilly.spacenerd.core.scaffold.navigation.HomeTab
-import com.adammcneilly.spacenerd.core.scaffold.navigation.components.BottomNavigationBar
 import com.adammcneilly.spacenerd.core.scaffold.navigation.components.SideNavigationRail
-import com.adammcneilly.spacenerd.test.paparazzi.BasePaparazziTest
 import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-class SideNavigationRailPaparazziTest : BasePaparazziTest() {
+@RunWith(TestParameterInjector::class)
+class SideNavigationRailPaparazziTest {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = app.cash.paparazzi.DeviceConfig.PIXEL_C,
+    )
+
+    @TestParameter
+    val useDarkTheme: Boolean = false
+
     @Test
     fun render() {
-        snapshot(screenPaddingDp = 0) {
+        paparazzi.snapshot {
             CompositionLocalProvider(
                 LocalAppState provides AppState(),
             ) {
-                SideNavigationRail()
+                SpaceTheme(
+                    darkTheme = useDarkTheme,
+                ) {
+                    SideNavigationRail()
+                }
             }
         }
     }
