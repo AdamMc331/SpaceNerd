8a5e9852bbc2570664d36a444c67a1e906163e98
Adam McNeilly <amcneilly331@gmail.com>
2025-08-20 20:21:40 -0400

    Adding station image and status to detail screen.

--- feature/stationdetail/src/commonMain/kotlin/com/adammcneilly/spacenerd/feature/stationdetail/StationDetailContent.kt
+++ feature/stationdetail/src/commonMain/kotlin/com/adammcneilly/spacenerd/feature/stationdetail/StationDetailContent.kt
@@ -1,10 +1,34 @@
 package com.adammcneilly.spacenerd.feature.stationdetail
 
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.adammcneilly.spacenerd.core.designsystem.components.ImageWrapper
+import com.adammcneilly.spacenerd.core.designsystem.components.Pill
+import com.adammcneilly.spacenerd.core.designsystem.utils.sharedBounds
+import com.adammcneilly.spacenerd.core.designsystem.utils.sharedElement
+import com.adammcneilly.spacenerd.core.displaymodels.LaunchDisplayModel
+import com.adammcneilly.spacenerd.core.displaymodels.SpaceStationDisplayModel
+import com.eygraber.compose.placeholder.PlaceholderDefaults
+import com.eygraber.compose.placeholder.material3.color
+import com.eygraber.compose.placeholder.material3.placeholder
+import com.eygraber.compose.placeholder.placeholder
+
+private const val STATION_IMAGE_ASPECT_RATIO = 1.5F
 
 @Composable
 fun StationDetailContent(
@@ -12,9 +36,124 @@ fun StationDetailContent(
     contentPadding: PaddingValues,
     modifier: Modifier = Modifier,
 ) {
+    LazyColumn(
+        contentPadding = PaddingValues(
+            bottom = 16.dp + contentPadding.calculateBottomPadding(),
+        ),
+        modifier = modifier,
+    ) {
+        item {
+            StationImageStatus(
+                state = state,
+                contentPadding = contentPadding,
+            )
+        }
+
+        item {
+            StationTitle(state)
+        }
+
+        // We should add a subtitle
+        // We could name the agency(s)
+//        item {
+//            LaunchSubtitle(state)
+//        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalSharedTransitionApi::class)
+private fun StationTitle(
+    state: StationDetailUiState,
+) {
     Text(
-        text = "Station Detail Stub: ${state.station}",
+        text = state.station.name,
+        style = MaterialTheme.typography.titleLarge,
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .padding(horizontal = 16.dp)
+            .placeholder(state.station.isPlaceholder)
+            .sharedBounds("STATION_TITLE_${state.station.id}"),
+    )
+}
+
+@Composable
+@OptIn(ExperimentalSharedTransitionApi::class)
+private fun StationSubtitle(
+    state: StationDetailUiState,
+) {
+    Text(
+        text = "TODO:",
+        style = MaterialTheme.typography.bodySmall,
+        modifier = Modifier
+            .padding(top = 4.dp)
+            .padding(horizontal = 16.dp)
+            .placeholder(state.station.isPlaceholder)
+            .sharedBounds("STATION_SUBTITLE_${state.station.id}"),
+    )
+}
+
+@Composable
+private fun StationImageStatus(
+    state: StationDetailUiState,
+    contentPadding: PaddingValues,
+) {
+    Box {
+        StationImage(
+            station = state.station,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(STATION_IMAGE_ASPECT_RATIO)
+                .sharedElement(
+                    key = "StationImage-${state.station.id}",
+                ),
+        )
+
+        StationStatus(
+            station = state.station,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = contentPadding.calculateTopPadding())
+                .padding(16.dp)
+                .placeholder(
+                    visible = state.station.isPlaceholder,
+                    color = PlaceholderDefaults.color(
+                        contentAlpha = 0.15F,
+                    ),
+                    shape = CircleShape,
+                )
+                .sharedElement(
+                    key = "StationStatus-${state.station.id}",
+                ),
+        )
+    }
+}
+
+@Composable
+private fun StationImage(
+    station: SpaceStationDisplayModel,
+    modifier: Modifier = Modifier,
+) {
+    ImageWrapper(
+        image = station.image,
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
         modifier = modifier
-            .padding(contentPadding),
+            .placeholder(station.isPlaceholder),
+    )
+}
+
+@Composable
+private fun StationStatus(
+    station: SpaceStationDisplayModel,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    Pill(
+        text = station.status.label,
+        containerColor = station.status.containerColor,
+        contentColor = station.status.contentColor,
+        textStyle = textStyle,
+        modifier = modifier,
     )
 }
