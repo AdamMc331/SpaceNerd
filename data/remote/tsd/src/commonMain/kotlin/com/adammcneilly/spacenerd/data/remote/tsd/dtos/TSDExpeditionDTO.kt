0648a8a9654efd23ab1ebb8e9cdb584d7bded4f4
Adam McNeilly <amcneilly331@gmail.com>
2025-09-10 20:12:49 -0400

    Adding expedition DTO.

---
+++ data/remote/tsd/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/tsd/dtos/TSDExpeditionDTO.kt
@@ -0,0 +1,29 @@
+package com.adammcneilly.spacenerd.data.remote.tsd.dtos
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TSDExpeditionDTO(
+    @SerialName("crew")
+    val crew: List<TSDCrewMemberDTO>? = null,
+    @SerialName("end")
+    val end: String? = null,
+    @SerialName("id")
+    val id: Int? = null,
+    @SerialName("mission_patches")
+    val missionPatches: List<TSDMissionPatchesDTO>? = null,
+    @SerialName("name")
+    val name: String? = null,
+    @SerialName("response_mode")
+    val responseMode: String? = null,
+    @SerialName("spacestation")
+    val spacestation: TSDSpaceStationSummaryDTO? = null,
+    // TODO: Support spacewalks
+//    @SerialName("spacewalks")
+//    val spacewalks: List<Any?>? = listOf(),
+    @SerialName("start")
+    val start: String? = null,
+    @SerialName("url")
+    val url: String? = null,
+)
