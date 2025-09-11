9bcd7733724ebb6b4d5b0f8d00518a02158c68fb
Adam McNeilly <amcneilly331@gmail.com>
2025-09-10 21:31:35 -0400

    Completing expeditions data layer.

--- data/expeditions/impl/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/expeditions/impl/OfflineFirstExpeditionRepository.kt
+++ data/expeditions/impl/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/expeditions/impl/OfflineFirstExpeditionRepository.kt
@@ -3,12 +3,32 @@ package com.adammcneilly.spacenerd.data.expeditions.impl
 import com.adammcneilly.spacenerd.core.models.Expedition
 import com.adammcneilly.spacenerd.data.expeditions.api.ExpeditionListRequest
 import com.adammcneilly.spacenerd.data.expeditions.api.ExpeditionRepository
+import com.adammcneilly.spacenerd.data.expeditions.api.local.LocalExpeditionService
+import com.adammcneilly.spacenerd.data.expeditions.api.remote.RemoteExpeditionService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.onStart
 
-class OfflineFirstExpeditionRepository : ExpeditionRepository {
+class OfflineFirstExpeditionRepository(
+    private val localExpeditionService: LocalExpeditionService,
+    private val remoteExpeditionService: RemoteExpeditionService,
+) : ExpeditionRepository {
     override fun getExpeditions(
         request: ExpeditionListRequest,
     ): Flow<List<Expedition>> {
-        TODO("Not yet implemented")
+        return localExpeditionService.getExpeditions(request)
+            .onStart {
+                val response = remoteExpeditionService.getExpeditions(request)
+
+                val expeditions = response.getOrNull()
+                if (expeditions != null) {
+                    localExpeditionService.saveExpeditions(expeditions)
+                }
+
+                val error = response.exceptionOrNull()
+                if (error != null) {
+                    // Need to log this somewhere
+                    println("Error fetching expeditions $request: $error")
+                }
+            }
     }
 }
