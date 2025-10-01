ab2fae3e963f3da3a3dc9ddb84bd0eb359973333
Adam McNeilly <amcneilly331@gmail.com>
2025-09-30 21:51:24 -0400

    Improving article caching. (#234)

--- data/article/impl/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/article/impl/local/room/RoomArticleService.kt
+++ data/article/impl/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/article/impl/local/room/RoomArticleService.kt
@@ -1,22 +1,34 @@
 package com.adammcneilly.spacenerd.data.article.impl.local.room
 
+import com.adammcneilly.spacenerd.core.datetime.DateTimeProvider
 import com.adammcneilly.spacenerd.core.models.Article
 import com.adammcneilly.spacenerd.data.article.api.local.LocalArticleService
 import com.adammcneilly.spacenerd.data.local.room.daos.RoomArticleDao
 import com.adammcneilly.spacenerd.data.local.room.dtos.RoomArticleDTO
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 /**
  * Implementation of [LocalArticleService] that will request data from a Room database via [articleDao].
  */
+@OptIn(ExperimentalTime::class)
 class RoomArticleService(
     private val articleDao: RoomArticleDao,
+    private val dateTimeProvider: DateTimeProvider,
 ) : LocalArticleService {
     override suspend fun saveArticles(
         articles: List<Article>,
     ) {
-        val dtoList = articles.map(::RoomArticleDTO)
+        val dtoList = articles.map { article ->
+            RoomArticleDTO(
+                article = article,
+                dateTimeProvider = dateTimeProvider,
+            )
+        }
 
         articleDao.insertArticles(dtoList)
     }
@@ -25,5 +37,24 @@ class RoomArticleService(
         return articleDao.getAllArticles().map { dtoList ->
             dtoList.map(RoomArticleDTO::toArticle)
         }
+    }
+
+    override suspend fun isCacheStale(): Boolean {
+        val cacheDuration = 12.hours
+
+        val latestCacheTimestamp = articleDao.getLatestCacheTimestamp()?.let {
+            Instant.parse(it)
+        }
+
+        // If no saved timestamp, consider cache stale and needs to be updated.
+        if (latestCacheTimestamp == null) {
+            return true
+        }
+
+        val now = dateTimeProvider.now()
+
+        val durationSinceCache = (now - latestCacheTimestamp)
+
+        return durationSinceCache > cacheDuration
     }
 }
--- data/local/room/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/local/room/daos/RoomArticleDao.kt
+++ data/local/room/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/local/room/daos/RoomArticleDao.kt
@@ -17,14 +17,22 @@ interface RoomArticleDao {
         articles: List<RoomArticleDTO>,
     )
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertArticle(
-        article: RoomArticleDTO,
+    @Query(
+        """
+        SELECT * 
+        FROM articles 
+        ORDER BY publishedAtUtc DESC 
+    """,
     )
-
-    @Query("SELECT * FROM articles ORDER BY publishedAtUtc DESC")
     fun getAllArticles(): Flow<List<RoomArticleDTO>>
 
-    @Query("DELETE FROM articles")
-    suspend fun clearAllArticles()
+    @Query(
+        """
+            SELECT lastPersistedAtUtc
+            FROM articles
+            ORDER BY lastPersistedAtUtc DESC
+            LIMIT 1
+        """,
+    )
+    suspend fun getLatestCacheTimestamp(): String?
 }
