197c96460d56f6816460d56ced38e22dd05e9ebc
Adam McNeilly <amcneilly331@gmail.com>
2025-12-17 23:37:40 -0500

    BaseURL update for TSD client.

--- data/remote/tsd/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/tsd/TSDKtorClient.kt
+++ data/remote/tsd/src/commonMain/kotlin/com/adammcneilly/spacenerd/data/remote/tsd/TSDKtorClient.kt
@@ -5,9 +5,9 @@ import com.adammcneilly.spacenerd.data.remote.ktor.BaseKtorClient
 /**
  * Implementation of [BaseKtorClient] that will make requests to the TSD API.
  *
- * Docs here: https://lldev.thespacedevs.com/2.3.0/
+ * Docs here: https://ll.thespacedevs.com/2.3.0/
  */
-object TSDKtorClient : BaseKtorClient("https://lldev.thespacedevs.com/2.3.0/") {
+object TSDKtorClient : BaseKtorClient("https://ll.thespacedevs.com/2.3.0/") {
     override fun baseParams(): Map<String, Any?> {
         return mapOf(
             "mode" to "detailed",
